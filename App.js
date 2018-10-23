7e8d98fa91a034fc507d941a7ec1671c2d3736c5
Kurt Miller <kmiller9393@gmail.com>
2018-10-23 16:45:12 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Route, NativeRouter, withRouter } from 'react-router-native';
-import  Home from './src/components/Home/Home';
-import  SpotMap from './src/components/SpotMap/SpotMap';
+import Home from './src/components/Home/Home';
+import SpotMap from './src/components/SpotMap/SpotMap';
 
 export default class App extends Component {
   render() {
     return (
       <NativeRouter>
         <View style={styles.container}>
-          <Route exact path="/" component={ Home }/>
-          <Route exact path="/SpotMap" component={ SpotMap } />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/SpotMap" component={SpotMap} />
         </View>
       </NativeRouter>
     );
@@ -22,16 +22,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
