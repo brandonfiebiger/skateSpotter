7ea617b57fef76d4695d40e6d954a944ad08f91e
Kurt Miller <kmiller9393@gmail.com>
2018-10-24 13:58:12 -0600

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { withRouter } from 'react-router-native';
-import { addSpot } from '../../actions/spots';
-import { connect } from 'react-redux';
 
 export class Home extends Component {
   constructor() {
@@ -17,11 +15,9 @@ export class Home extends Component {
 
   handleRouteToMap = () => {
     this.props.history.push('/SpotMap');
-    this.props.addASpot('hello')
   };
 
   render() {
-    console.log(this.props);
     return (
       <View>
         <Text>SkateSpotter</Text>
@@ -32,8 +28,4 @@ export class Home extends Component {
   }
 }
 
-export const mapDispatchToProps = dispatch => ({
-  addASpot: spot => dispatch(addSpot(spot))
-})
-
-export default withRouter(connect(null, mapDispatchToProps)(Home));
+export default withRouter(Home);
