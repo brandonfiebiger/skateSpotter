6b4dfec6797fa87542c8831b06a2a1ecefabb92c
Kurt Miller <kmiller9393@gmail.com>
2018-10-25 13:45:17 -0600

    Add handleGetLocation to Home container that gets user's current location

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-native';
+import { getUserLocation } from '../../store/actions';
 
 export class Home extends Component {
   constructor() {
@@ -11,21 +13,50 @@ export class Home extends Component {
   }
   handlePress = () => {
     this.handleRouteToMap();
+    this.handleGetLocation();
   };
 
   handleRouteToMap = () => {
     this.props.history.push('/SpotMap');
   };
 
+  handleGetLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        let location = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        };
+        this.props.getLocation(location);
+      },
+      err => console.log(err)
+    );
+  };
+
   render() {
     return (
       <View>
         <Text>SkateSpotter</Text>
-        <Button title="Spots Near Me!" onPress={this.handlePress} />
+        <Button title="Spots Near Me!" onPress={() => this.handlePress()} />
         <Button title="Login" />
       </View>
     );
   }
 }
 
-export default withRouter(Home);
+export const mapStateToProps = state => ({
+  userLocation: state.userLocation
+});
+
+export const mapDispatchToProps = dispatch => ({
+  getLocation: location => dispatch(getUserLocation(location))
+});
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Home)
+);
