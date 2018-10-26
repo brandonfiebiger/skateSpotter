9db772de2cb07dbf40ce75689f789d4ef6ba65ce
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-25 19:46:20 -0600

    Add password validation method

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,13 +1,16 @@
-const validate = (val, rules) => {
+const validate = (val, rules, password) => {
   let isValid = true;
   for (let rule in rules) {
     switch (rule) {
       case 'minLength':
-        isValid = isValid && minLengthValidator(val, rules[rule])
+        isValid = isValid && minLengthValidator(val, rules[rule]);
         break;
       case 'isEmail':
         isValid = isValid && emailValidator(val, rules[rule]);
         break;
+      case 'equalTo':
+        isValid = isValid && passwordValidator(val, password);
+        break
       default:
         isValid = true;
     }
@@ -23,6 +26,14 @@ const minLengthValidator = (val, minLength) => {
   if (val.length >= minLength) {
     return true;
   } else { 
+    return false;
+  }
+}
+
+const passwordValidator = (confirmPassword, password) => {
+  if (confirmPassword === password) {
+    return true;
+  } else {
     return false;
   }
 }
