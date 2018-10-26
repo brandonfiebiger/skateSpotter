297a71ce9a04b794b11264ca5b17531b142dacb2
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-25 18:23:40 -0600

    Make SignUp into a controlled form component, add rules for user validation

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import validate from '../../utils/validation';
 
 class SignUp extends Component {
   constructor() {
@@ -14,20 +15,51 @@ class SignUp extends Component {
           isEmail: true
         }
       },
-      password,
-      confirmPassword
+      password: {
+        value: "",
+        valid: false,
+        validationRules: {
+          minLength: 6
+        }
+      },
+      passwordConfirmation: {
+        value: "",
+        valid: false,
+        validationRules: {
+          equalTo: 'password'
+        }
+      }
     }
   }
 
+  handleOnChange = (key, value) => {
+    this.setState(prevState => {
+      return {
+        [key]: {
+          ...prevState[key],
+          valid: validate(value, prevState[key].validationRules),
+          value
+        }
+      };
+    });
+  };
 
   render() {
     return (
       <View>
-        <TextInput value={this.state.email.value} />
-        <TextInput value={this.state.password.value} />
-        <TextInput value={this.state.confirmPassword.value} />
+        <TextInput value={this.state.email.value} onChangeText={val => this.handleOnChange('email', val)}/>
+        <TextInput value={this.state.password.value} onChangeText={val => this.handleOnChange('password', val)}/>
+        <TextInput value={this.state.passwordConfirmation.value} onChangeText={val => this.handleOnChange('passwordConfirmation', val)}/>
         <Button title="Sign Up" onPress={this.handleSubmit} />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    
+  }
+})
+
+export default SignUp;
