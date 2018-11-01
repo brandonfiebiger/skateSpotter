85b00b0767971600e94c8fbacc8056dfa5280b56
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-31 18:04:06 -0600

    Add fetch call to get currently logged in user from app and conditionally render side bar options

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import { connect } from 'react-redux';
 import SpotForm from './src/components/SpotForm/SpotForm';
 import SignUp from './src/components/SignUp/SignUp';
 import Login from './src/components/Login/Login';
-import { populateSpots } from './src/store/actions';
+import { populateSpots, logIn } from './src/store/actions';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation';
 import Avatar from './src/assets/images/avatar-example.png';
 import LogOut from './src/components/LogOut/LogOut';
@@ -25,33 +25,44 @@ export class App extends Component {
       .then(response => response.json())
       .then(spots => this.props.populateSpots(spots))
       .catch(error => console.log(error));
+
+      fetch('https://skate-spotter.herokuapp.com/api/v1/skater_page')
+        .then(response => response.json())
+        .then(user => this.props.login(user))
+        .catch(error => console.log(error));
   }
 
   render() {
-    return <Drawer />;
+    return this.props.currentUser ? <LoggedInDrawer /> : <LoggedOutDrawer />
   }
 }
 
-const Drawer = createDrawerNavigator(
+const LoggedOutDrawer = createDrawerNavigator (
   {
     Home: Home,
     'Spots Near Me': SpotMap,
     Login: Login,
     'Sign Up': SignUp,
+    
+  } 
+);
+
+const LoggedInDrawer = createDrawerNavigator(
+  {
+    'Spots Near Me': SpotMap,
     'Spot It': SpotForm,
     'Logout': LogOut
-  },
-  {
   }
 );
 
 export const mapStateToProps = state => ({
-  spots: state.spots
+  spots: state.spots,
+  currentUser: state.currentUser
 });
 
 export const mapDispatchToProps = dispatch => ({
   populateSpots: spots => dispatch(populateSpots(spots)),
-  logout: () => dispatch(logout())
+  login: (user) => dispatch(logIn(user))
 });
 
 export default connect(
